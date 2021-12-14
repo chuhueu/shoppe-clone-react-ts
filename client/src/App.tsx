5d49afddba393b2e1186c53c2198d58c66bcb635
Hueu123 <chuhueu1@gmail.com>
2021-12-14 11:39:55 +0700

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
+import { routes } from "./routes/routes";
+import { Header, Footer } from "./components/index";
 function App() {
   return (
-    <div className="App">
-      <h1>this is app</h1>
+    <div className="wrapper">
+      <Router>
+        <Header />
+        {renderRoutes(routes)}
+        <Footer />
+      </Router>
     </div>
   );
 }
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-    return (
-        <div>
+  return <div>this is the homepage</div>;
+};
 
-        </div>
-    )
-}
-
-export default HomePage
+export default HomePage;
