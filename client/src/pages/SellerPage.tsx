eb871a1e1363fb250d4bb7bcb7f4e788470276b6
Hueu123 <chuhueu1@gmail.com>
2021-12-31 16:50:23 +0700

    seller page

--- client/src/pages/SellerPage.tsx
+++ client/src/pages/SellerPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Header, Footer } from "../components/index";
+import { Box } from "@material-ui/core";
 const SellerPage = () => {
   return (
     <div>
       <Header />
-      <h1>this is the seller page</h1>
+      <Box mt={2} component="h1">
+        This is the Seller page
+      </Box>
       <Footer show={true} />
     </div>
   );
