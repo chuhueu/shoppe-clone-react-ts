cbc36e2c10af060d4988e2e41340b13575386370
Hueu123 <chuhueu1@gmail.com>
2022-01-21 10:20:18 +0700

    commit

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import App2 from "./App2";
 import { ThemeProvider } from "@material-ui/styles";
 import { theme } from "./theme";
 import { Provider } from "react-redux";
